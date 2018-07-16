127a006283f8e8465979d247f870f76b156ab543
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-16 00:34:59 -0300

    Merge master to develop

--- db/migrate/20180715182339_add_username_to_system_user.rb
+++ db/migrate/20180715182339_add_username_to_system_user.rb
@@ -1,5 +1,5 @@
 class AddUsernameToSystemUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :system_users, :username, :string , null: false, unique: true
+    add_column :system_users, :username, :string , null: false
   end
 end
