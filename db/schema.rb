26c80da485e6969a162e3e48a52f8e4360a91b1b
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-21 14:50:38 -0300

    adjusting categories migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_15_183037) do
+ActiveRecord::Schema.define(version: 2018_07_21_172057) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "description", limit: 100, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "master_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +33,29 @@ ActiveRecord::Schema.define(version: 2018_07_15_183037) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_master_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_master_users_on_reset_password_token", unique: true
+  end
+
+  create_table "people", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "kind", limit: 20, null: false
+    t.string "name", limit: 100, default: ""
+    t.string "company_name", limit: 200, default: ""
+    t.string "person_rg", default: ""
+    t.string "personC_ie", default: ""
+    t.string "person_cpf", default: ""
+    t.string "personC_cnpj", default: ""
+    t.string "address", limit: 100, default: ""
+    t.string "address_number", limit: 50, default: ""
+    t.string "address_complement", limit: 100, default: ""
+    t.string "address_bairro", limit: 100, default: ""
+    t.bigint "address_zip", null: false
+    t.string "address_city", limit: 100, null: false
+    t.string "address_state", limit: 50, null: false
+    t.bigint "person_tel1", null: false
+    t.bigint "person_tel2"
+    t.string "person_email", limit: 100, default: ""
+    t.string "person_obs", limit: 200, default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "system_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
