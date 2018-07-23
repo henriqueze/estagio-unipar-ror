9983559b45fe7c7a06e67e5cebf37caf06452a60
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-22 22:10:56 -0300

    Scaffold and migration for sales

---
+++ db/migrate/20180723010613_create_sales.rb
@@ -0,0 +1,13 @@
+class CreateSales < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sales do |t|
+      t.datetime :date, null: false
+      t.decimal :total_value, null: false
+      t.decimal :discount_value, default: 0
+      t.string :state, limit: 20, null: false, default: "Aberta"
+      t.references :person, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
