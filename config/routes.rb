e48ea0271bc58be8fb13dae9d8e1074b88fac89b
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-22 22:31:27 -0300

    Scaffold and migrations for purchases, add timestamp to sales

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :purchases
   resources :budgets
   resources :sales
   resources :payment_types
--- db/migrate/20180723012931_create_purchases.rb
+++ db/migrate/20180723012931_create_purchases.rb
@@ -1,10 +1,9 @@
-class CreateSales < ActiveRecord::Migration[5.2]
+class CreatePurchases < ActiveRecord::Migration[5.2]
   def change
-    create_table :sales do |t|
+    create_table :purchases do |t|
       t.datetime :date, null: false, default: -> { 'CURRENT_TIMESTAMP' }
       t.decimal :total_value, null: false
-      t.decimal :discount_value, default: 0
-      t.string :state, limit: 20, null: false, default: "Aberta"
+      t.decimal :freight_value, default: 0
       t.references :person, foreign_key: true
 
       t.timestamps
