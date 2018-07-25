f8661956d865ccf6c72b6174b071dcd916acc1f9
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-25 10:02:12 -0300

    Associations and search for model category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,17 @@
 class Category < ApplicationRecord
+	has_many :product
+
+	validates :description, uniqueness: { case_sensitive: false}, presence: true,
+				length: { minimum: 2 }
+	validates :description, format: { with: /\A[a-zA-Z\s]+\z/,
+				message: "Aceita Somente Letras" }
+
+	def self.search(search)
+		if search
+			where(['description LIKE ?', "%#{search}%"])
+		else
+			all
+		end
+	end
+
 end
