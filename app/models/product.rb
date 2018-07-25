0ea9733840e61e6c4aabc821ff616226a001010e
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-25 10:18:32 -0300

    Associations for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
+  #venda
+  has_many :item_sale
+  has_many :sale, through: :item_sale
+  #orçamento
+  has_many :item_budget
+  has_many :budget, through: :item_budget
+  #compra
+  has_many :item_purchase
+  has_many :purchase, through: :item_purchase
 end
