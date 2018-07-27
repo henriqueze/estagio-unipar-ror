57cdff9b3e39b9527bb1a917d5036d5db1599cd1
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-26 23:43:58 -0300

    Setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   devise_for :system_users
   devise_for :master_users
   get 'home/index'
-  root 'home#index'
+  root 'site/home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
