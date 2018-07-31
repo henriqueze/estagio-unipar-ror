1772d15acfea1b52986d613e81eb9496a2f1625d
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-07-31 00:12:18 -0300

    Person layout and views

---
+++ app/helpers/backoffice/people_helper.rb
@@ -0,0 +1,2 @@
+module Backoffice::PeopleHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
+
   get 'admin', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
     resources :categories, except: [:show, :destroy]
+    resources :people, except: [ :destroy]
     get 'categories/index'
+    get 'people/index'
     get 'dashboard', to: 'dashboard#index'
   end
 
@@ -18,10 +21,9 @@ Rails.application.routes.draw do
   resources :sales
   resources :payment_types
   resources :products
-  resources :people
 
   devise_for :system_users
-  devise_for :master_users, :skip =>[:registrations]
+  devise_for :master_users
 
   get 'home/index'
   root 'site/home#index'
