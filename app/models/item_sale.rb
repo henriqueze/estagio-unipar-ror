57121c8dc0d9e3fd2d2d5c3225a092ffb28292d3
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-08-07 09:04:31 -0300

    validators

--- app/models/item_sale.rb
+++ app/models/item_sale.rb
@@ -1,5 +1,12 @@
 class ItemSale < ApplicationRecord
-  belongs_to :product
-  belongs_to :sale
+	belongs_to :product
+	belongs_to :sale
+
+	validates :amount, numericality: { greater_than_or_equal_to: 1 }
+
+	def subtotal
+		product.sale_price * amount
+	end
+end
 
 end
