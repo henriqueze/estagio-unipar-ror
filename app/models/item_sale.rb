d55cb6f9575f940bdc6796f5555ecd87af5e4aaa
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-08-07 09:06:16 -0300

    validator for item_sale

--- app/models/item_sale.rb
+++ app/models/item_sale.rb
@@ -2,6 +2,7 @@ class ItemSale < ApplicationRecord
 	belongs_to :product
 	belongs_to :sale
 
+	validates :product_id, presence: true
 	validates :amount, numericality: { greater_than_or_equal_to: 1 }
 
 	def subtotal
