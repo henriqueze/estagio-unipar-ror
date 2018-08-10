ebe58c2adf90c68215a53b1193134709a56c0f79
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-08-09 21:20:50 -0300

    config budgets

--- app/controllers/backoffice/budgets_controller.rb
+++ app/controllers/backoffice/budgets_controller.rb
@@ -15,6 +15,8 @@ class Backoffice::BudgetsController < BackofficeController
 		if @budget.save
 			redirect_to backoffice_budgets_path,
 			notice: "Orçamento #{@budget.id} Cadastrado com Sucesso"
+			@budget.atualiza_estoque_diminui
+			@budget.atualiza_estoque_reservado_aumenta
 		else
 			render :new
 		end
@@ -43,6 +45,7 @@ class Backoffice::BudgetsController < BackofficeController
 
 	def params_budget
 		params.require(:budget).permit(:kind, :note, :date, :expiration_date,
-											 :total_value, :state, :person_id)
+			:total_value, :state, :person_id,
+			 item_budgets_attributes: [:id, :amount, :value, :product_id, :budget_id]))
 	end
 end
