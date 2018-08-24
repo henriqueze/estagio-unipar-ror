5f0902586b7e8b00947b65f25f40d3a79fba4a08
DESKTOP-9I535E1\henri <Henrique.ze@live.com>
2018-08-24 10:32:22 -0300

    ransack and paginate

--- app/controllers/backoffice/accounts_payables_controller.rb
+++ app/controllers/backoffice/accounts_payables_controller.rb
@@ -2,7 +2,14 @@ class Backoffice::AccountsPayablesController < BackofficeController
 	before_action :set_accounts_payable, only: [:edit, :update, :show]
 
 	def index
-		@accounts_payables = AccountsPayable.all
+		@q = AccountsPayable.ransack(params[:q])
+		@accounts_payables = @q.result.page(params[:page]).per(10)
+		@q.build_condition if @q.conditions.empty?
+
+		respond_to do |format|
+			format.html
+			format.pdf {@q.result}
+		end
 	end
 
 	#before_action :authenticate_system_user! verificar depois porque não está passando o sql correto
@@ -43,10 +50,10 @@ class Backoffice::AccountsPayablesController < BackofficeController
 
 	def params_accounts_payable
 		params.require(:accounts_payable).permit(:state, :description, :document_kind,
-						 :document_number, :issue_date, :expiration_date,
-						 :payment_date, :total_parcels, :current_parcel, :payable_value,
-						 :additions_value, :discount_value, :total_value, :paid_value,
-						  :voucher_image, :purchase_id, :provider_id)
+			:document_number, :issue_date, :expiration_date,
+			:payment_date, :total_parcels, :current_parcel, :payable_value,
+			:additions_value, :discount_value, :total_value, :paid_value,
+			:voucher_image, :purchase_id, :provider_id)
 	end
 end
 
